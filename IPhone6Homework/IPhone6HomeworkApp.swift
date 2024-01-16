b27114b5849c28a924d18deb0248c94db0d815eb
Ivan Sabev <ivantmm@abv.bg>
2024-01-16 14:13:07 +0200

    Initial Commit

---
+++ IPhone6Homework/IPhone6HomeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  IPhone6HomeworkApp.swift
+//  IPhone6Homework
+//
+//  Created by Ivan Sabev on 16.01.24.
+//
+
+import SwiftUI
+
+@main
+struct IPhone6HomeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
